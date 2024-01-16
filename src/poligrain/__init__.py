ab067b9884cf645864354e0cbb002c3cf82ce403
Christian Chwala <christian.chwala@kit.edu>
2024-01-16 10:13:43 +0100

    hack to solve problem with netcdf4 and numpy compatibility
    problem si described in https://github.com/pydata/xarray/issues/7259

--- src/poligrain/__init__.py
+++ src/poligrain/__init__.py
@@ -7,6 +7,13 @@ poligrain: Effortlessly plot and compare (rainfall) sensor data with point, line
 
 from __future__ import annotations
 
+# This is only here to suppress the bug described in
+# https://github.com/pydata/xarray/issues/7259
+# We have to make sure that netcdf4 is imported before
+# numpy is imported for the first time, e.g. also via
+# importing xarray
+import netCDF4  # noqa: F401
+
 __version__ = "0.0.0"
 
 from . import plot_map
