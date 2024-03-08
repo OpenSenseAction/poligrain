833b4e15c04735ab9024f40c5ed1005cf64d1e6f
Christian Chwala <christian.chwala@kit.edu>
2024-03-08 12:23:38 +0100

    refactoring to have a low-level plotting function `scatter_lines`

--- src/poligrain/plot_map.py
+++ src/poligrain/plot_map.py
@@ -4,9 +4,70 @@ import matplotlib.axes
 import matplotlib.patheffects as pe
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.typing as npt
 import xarray as xr
 from matplotlib.collections import LineCollection
 from matplotlib.colors import Colormap, Normalize
+
+
+def scatter_lines(
+    x0: np.ndarray | npt.ArrayLike | float,
+    x1: np.ndarray | npt.ArrayLike | float,
+    y1: np.ndarray | npt.ArrayLike | float,
+    y0: np.ndarray | npt.ArrayLike | float,
+    s: float = 3,
+    c: str | np.array = "C0",
+    line_style: str = "-",
+    pad_width: float = 1,
+    cap_style: str = "round",
+    vmin: float | None = None,
+    vmax: float | None = None,
+    cmap: (str | Colormap) = "viridis",
+    ax: (matplotlib.axes.Axes | None) = None,
+) -> LineCollection:
+    if ax is None:
+        _, ax = plt.subplots()
+
+    x0 = np.atleast_1d(x0)
+    y0 = np.atleast_1d(y0)
+    x1 = np.atleast_1d(x1)
+    y1 = np.atleast_1d(y1)
+
+    data = None if isinstance(c, str) else c
+
+    if data is None:
+        lines = LineCollection(
+            [((x0[i], y0[i]), (x1[i], y1[i])) for i in range(len(x0))],
+            linewidth=s,
+            linestyles=line_style,
+            color=c,
+        )
+
+    else:
+        if vmax is None:
+            vmax = np.nanmax(data)
+        if vmin is None:
+            vmin = np.nanmin(data)
+        norm = Normalize(vmin=vmin, vmax=vmax)
+        lines = LineCollection(
+            [((x0[i], y0[i]), (x1[i], y1[i])) for i in range(len(x0))],
+            norm=norm,
+            cmap=cmap,
+            linewidth=s,
+            linestyles=line_style,
+            capstyle=cap_style,
+            path_effects=[
+                pe.Stroke(linewidth=s + pad_width, foreground="k", capstyle=cap_style),
+                pe.Normal(),
+            ],
+        )
+        lines.set_array(data)
+
+    ax.add_collection(lines)
+    # This is required because x and y bounds are not adjusted after adding the `lines`.
+    ax.autoscale()
+
+    return lines
 
 
 def plot_lines(
@@ -58,49 +119,27 @@ def plot_lines(
         _, ax = plt.subplots()
 
     try:
-        data = cmls.data
-        if len(data.shape) != 1:
-            msg = f"If you pass an xarray.DataArray it has to be 1D, with the length of the cml_id dimension. You passed in something with shape {data.shape}"
+        color_data = cmls.data
+        if len(color_data.shape) != 1:
+            msg = (
+                f"If you pass an xarray.DataArray it has to be 1D, with the length of the "
+                f"cml_id dimension. You passed in something with shape {color_data.shape}"
+            )
             raise ValueError(msg)
     except AttributeError:
-        data = None
+        color_data = line_color
 
-    x0 = np.atleast_1d(cmls.site_0_lon.values)
-    y0 = np.atleast_1d(cmls.site_0_lat.values)
-    x1 = np.atleast_1d(cmls.site_1_lon.values)
-    y1 = np.atleast_1d(cmls.site_1_lat.values)
-
-    if data is None:
-        lines = LineCollection(
-            [((x0[i], y0[i]), (x1[i], y1[i])) for i in range(len(x0))],
-            linewidth=line_width,
-            color=line_color,
-        )
-
-    else:
-        if vmax is None:
-            vmax = np.nanmax(data)
-        if vmin is None:
-            vmin = np.nanmin(data)
-        norm = Normalize(vmin=vmin, vmax=vmax)
-        lines = LineCollection(
-            [((x0[i], y0[i]), (x1[i], y1[i])) for i in range(len(x0))],
-            norm=norm,
-            cmap=cmap,
-            linewidth=line_width,
-            linestyles="solid",
-            capstyle="round",
-            path_effects=[
-                pe.Stroke(
-                    linewidth=line_width + pad_width, foreground="k", capstyle="round"
-                ),
-                pe.Normal(),
-            ],
-        )
-        lines.set_array(data)
-
-    ax.add_collection(lines)
-    # This is required because x and y bounds are not adjusted after adding the `lines`.
-    ax.autoscale()
-
-    return lines
+    return scatter_lines(
+        x0=cmls.site_0_lon.values,
+        y0=cmls.site_0_lat.values,
+        x1=cmls.site_1_lon.values,
+        y1=cmls.site_1_lat.values,
+        s=line_width,
+        c=color_data,
+        pad_width=pad_width,
+        line_style="-",
+        ax=ax,
+        vmin=vmin,
+        vmax=vmax,
+        cmap=cmap,
+    )
