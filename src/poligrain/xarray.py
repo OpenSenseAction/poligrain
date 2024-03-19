c7b489239071c1988bf5174b9fac1cafac714bc9
Christian Chwala <christian.chwala@kit.edu>
2024-03-19 09:41:51 +0100

    linting

--- src/poligrain/xarray.py
+++ src/poligrain/xarray.py
@@ -1,3 +1,5 @@
+"""xarray Accessors."""
+
 import matplotlib.axes
 import xarray as xr
 from matplotlib.collections import LineCollection
@@ -8,7 +10,7 @@ from . import plot_map
 
 @xr.register_dataset_accessor("plg")
 class PoligrainDatasetAccessor:
-    """Accessor for functionality of poligrain"""
+    """Accessor for functionality of poligrain."""
 
     def __init__(self, dataset: xr.Dataset):
         self._dataset = dataset
@@ -25,16 +27,18 @@ class PoligrainDatasetAccessor:
         Parameters
         ----------
         line_color : str, optional
-            The color of the lines when plotting based on a `xarray.Dataset`, by default "k".
+            The color of the lines when plotting based on a `xarray.Dataset`,
+            by default "k".
         line_width : float, optional
             The width of the lines. In case of coloring lines with a `cmap`, this is the
-            width of the colored line, which is extend by `pad_width` with a black outline.
-            By default 1.
+            width of the colored line, which is extend by `pad_width` with
+            a black outline. By default 1.
         pad_width : float, optional
-            The width of the outline in black around the lines colored via a `cmap`, by default 1.
+            The width of the outline in black around the lines colored via a `cmap`,
+            by default 1.
         ax : matplotlib.axes.Axes  |  None, optional
-            A `Axes` object on which to plot. If not supplied, a new figure with an `Axes`
-            will be created. By default None.
+            A `Axes` object on which to plot. If not supplied, a new figure with
+            an `Axes` will be created. By default None.
 
         Returns
         -------
@@ -52,7 +56,7 @@ class PoligrainDatasetAccessor:
 
 @xr.register_dataarray_accessor("plg")
 class PoligrainDataArrayAccessor:
-    """Accessor for functionality of poligrain"""
+    """Accessor for functionality of poligrain."""
 
     def __init__(self, data_array: xr.DataArray):
         self._data_array = data_array
@@ -76,18 +80,21 @@ class PoligrainDataArrayAccessor:
         vmax : float  |  None, optional
             Maximum value of colormap, by default None.
         cmap : str  |  Colormap, optional
-            A matplotlib colormap either as string or a `Colormap` object, by default "turbo".
+            A matplotlib colormap either as string or a `Colormap` object,
+            by default "turbo".
         line_color : str, optional
-            The color of the lines when plotting based on a `xarray.Dataset`, by default "k".
+            The color of the lines when plotting based on a `xarray.Dataset`,
+            by default "k".
         line_width : float, optional
             The width of the lines. In case of coloring lines with a `cmap`, this is the
-            width of the colored line, which is extend by `pad_width` with a black outline.
-            By default 1.
+            width of the colored line, which is extend by `pad_width` with
+            a black outline. By default 1.
         pad_width : float, optional
-            The width of the outline in black around the lines colored via a `cmap`, by default 1.
+            The width of the outline in black around the lines colored via a `cmap`,
+            by default 1.
         ax : matplotlib.axes.Axes  |  None, optional
-            A `Axes` object on which to plot. If not supplied, a new figure with an `Axes`
-            will be created. By default None.
+            A `Axes` object on which to plot. If not supplied, a new figure with
+            an `Axes` will be created. By default None.
 
         Returns
         -------
