976536b5ab3c5bba521b01aa0b32559487257d0c
Christian Chwala <christian.chwala@kit.edu>
2024-10-24 08:42:39 +0200

    extend tests, also delete datadir before and after to check conditions of download function

--- tests/test_example_data.py
+++ tests/test_example_data.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 
 import numpy as np
@@ -14,6 +15,10 @@ def test_download_data_file():
     fn = "openmrg_municp_gauge_5min_2h.nc"
     url = f"{BASE_URL}/raw/{VERSION}/OpenMRG/{fn}"
     data_dir = "example_data"
+
+    # Make sure the data directory is not there
+    shutil.rmtree(data_dir, ignore_errors=True)
+
     plg.example_data.download_data_file(
         url=url, local_file_name=fn, local_path=data_dir
     )
@@ -22,13 +27,20 @@ def test_download_data_file():
     npt.assert_almost_equal(
         ds_gauges.lon.data[:3], np.array([11.943145, 12.035572, 12.073303])
     )
-    npt.assert_almost_equal(
-        ds_gauges.station_id.data[:3],
-        np.array(
-            [
-                0,
-                1,
-                2,
-            ]
-        ),
+    npt.assert_almost_equal(ds_gauges.station_id.data[:3], np.array([0, 1, 2]))
+
+    # call download again with printing info enabled, which I do not know
+    # how to test (and it is not important) but we cover this one if-statement
+    return_code = plg.example_data.download_data_file(
+        url=url,
+        local_file_name=fn,
+        local_path=data_dir,
+        print_output=True,
     )
+    # since this was the second download of the same file with the same target
+    # on the local storage, now file was downloaded and the function should
+    # return None
+    assert return_code is None
+
+    # Delete the data directory with the downloaded files
+    shutil.rmtree(data_dir)
