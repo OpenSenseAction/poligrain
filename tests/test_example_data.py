b3cd7e4f02fcc968a023557c57c7c17ac12368b4
Christian Chwala <christian.chwala@kit.edu>
2024-10-24 10:26:37 +0200

    add test for load_openmrg_5min_2h

--- tests/test_example_data.py
+++ tests/test_example_data.py
@@ -50,3 +50,46 @@ def test_download_data_file():
 
         # close file, because otherwise CI on Windows fails when trying to delete dir
         ds_gauges.close()
+
+
+def test_load_openmrg_5min_2h():
+    # We download the data and just check a little bit of the data. Here
+    # we do not yet check that data format conventions are correct.
+    with tempfile.TemporaryDirectory() as tmp_dir_name:
+        (
+            ds_rad,
+            ds_cmls,
+            ds_gauges_municp,
+            ds_gauge_smhi,
+        ) = plg.example_data.load_openmrg_5min_2h(data_dir=tmp_dir_name)
+
+        # Check radar data
+        npt.assert_almost_equal(
+            ds_rad.rainfall_amount.data[10, 20:22, 15:18],
+            np.array(
+                [
+                    [0.03038611, 0.02556672, 0.02556672],
+                    [0.04546478, 0.03409377, 0.04292152],
+                ]
+            ),
+        )
+
+        # Check CML data
+        npt.assert_almost_equal(
+            ds_cmls.isel(cml_id=42).R.data[11:14],
+            np.array([0.58149174, 0.43223663, 0.33730422]),
+        )
+
+        # Check content of muncip gauge data
+        npt.assert_almost_equal(
+            ds_gauges_municp.lon.data[:3], np.array([11.943145, 12.035572, 12.073303])
+        )
+        npt.assert_almost_equal(
+            ds_gauges_municp.station_id.data[:3], np.array([0, 1, 2])
+        )
+
+        # Check data from SMHI gauge
+        npt.assert_almost_equal(
+            ds_gauge_smhi.rainfall_amount.isel(station_id=0).data[11:14],
+            np.array([0.63333333, 0.63333333, 0.53333333]),
+        )
