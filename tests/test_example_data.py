370133e5413c8ac91e0581b3f2fb817f61b9678f
Christian Chwala <christian.chwala@kit.edu>
2024-10-24 09:14:53 +0200

    improved test coverage, hopefully

--- tests/test_example_data.py
+++ tests/test_example_data.py
@@ -1,4 +1,3 @@
-import shutil
 import tempfile
 from pathlib import Path
 
@@ -20,28 +19,31 @@ def test_download_data_file():
     with tempfile.TemporaryDirectory() as tmp_dir_name:
         data_dir_in_tmp_dir = Path(tmp_dir_name) / data_dir
 
+        # test download with specific local file name
         plg.example_data.download_data_file(
-            url=url, local_file_name=fn, local_path=data_dir_in_tmp_dir
+            url=url,
+            local_file_name="some_file_name.nc",
+            local_path=data_dir_in_tmp_dir,
+            print_output=True,
         )
-        ds_gauges = xr.open_dataset(Path(data_dir_in_tmp_dir) / fn)
+        ds_gauges = xr.open_dataset(Path(data_dir_in_tmp_dir) / "some_file_name.nc")
 
         npt.assert_almost_equal(
             ds_gauges.lon.data[:3], np.array([11.943145, 12.035572, 12.073303])
         )
         npt.assert_almost_equal(ds_gauges.station_id.data[:3], np.array([0, 1, 2]))
 
-        # call download again with printing info enabled, which I do not know
-        # how to test (and it is not important) but we cover this one if-statement
+        # call download without specifying local file name and check that
+        # file gets correct filename which is the last part of the URL
+        # which is `fn` since we build it like that above.
         return_code = plg.example_data.download_data_file(
             url=url,
-            local_file_name=fn,
-            local_path=data_dir_in_tmp_dir,
-            print_output=True,
         )
-        # since this was the second download of the same file with the same target
-        # on the local storage, now file was downloaded and the function should
-        # return None
-        assert return_code is None
+        assert Path(fn).exists()
 
-        # Delete the data directory with the downloaded files
-        shutil.rmtree(data_dir_in_tmp_dir)
+        # download again to the same local target, which should not do anything and
+        # then return None
+        return_code = plg.example_data.download_data_file(
+            url=url,
+        )
+        assert return_code is None
